22e7e506fb9d3bdcc88e320936111840fdb23354
Rishin Goswami <rishin.goswami@gmail.com>
2020-06-23 15:21:15 +0530

    Testing framework added

---
+++ cmake/googletest.cmake
@@ -0,0 +1,27 @@
+configure_file(cmake/googletest-CMakeLists.txt.in googletest-download/CMakeLists.txt)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
+
+if(result)
+  message(FATAL_ERROR "CMake step for googletest failed: ${result}")
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
+
+if(result)
+  message(FATAL_ERROR "Build step for googletest failed: ${result}")
+endif()
+
+# Prevent overriding the parent project's compiler/linker settings on Windows
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+#  target_compile_features(gtest PUBLIC cxx_std_17)
+#  target_compile_features(gtest_main PUBLIC cxx_std_17)
+
+# Add googletest directly to our build. This defines the gtest and gtest_main targets.
+add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/googletest-src
+                 ${CMAKE_CURRENT_BINARY_DIR}/googletest-build
+                 EXCLUDE_FROM_ALL)
---
+++ test/dummy_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(dummy, dummy_basic) {
+    EXPECT_TRUE(true);
+    EXPECT_FALSE(false);
+}
+
